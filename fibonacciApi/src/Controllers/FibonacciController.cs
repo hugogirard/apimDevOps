886dddf31e7a8fc97eb6aa10b78f17030bdcc8c0
Hugo Girard <hugogirard28@hotmail.com>
2021-07-06 21:33:20 -0400

    added fibonacciApi

---
+++ fibonacciApi/src/Controllers/FibonacciController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace src.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+        private readonly ILogger<FibonacciController> _logger;
+
+        public FibonacciController(ILogger<FibonacciController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get(int len) 
+        {
+            var sequence = new List<int>();
+            int a = 0, b = 1, c = 0;   
+            for (int i = 2; i < len; i++)  
+            {  
+                c= a + b;  
+                sequence.Add(c); 
+                a= b;  
+                b= c;  
+            }
+
+            return sequence;
+        }
+    }
+}
