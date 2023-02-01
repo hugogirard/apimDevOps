5bf4f8c7df2da935e39645c64eef06c39125d849
hugogirard <hugo.girard@microsoft.com>
2023-02-01 09:54:08 -0500

    move to bicep

--- fibonacciApi/src/Startup.cs
+++ fibonacciApi/src/Startup.cs
@@ -29,7 +29,7 @@ namespace src
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
-            {
+            {                
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fibonacci API", Version = "v1" });
             });
         }
@@ -42,8 +42,12 @@ namespace src
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger(o => o.SerializeAsV2 = true);
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fibonacci API v1"));
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.yaml", "Fibonacci API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
